df3e7f5e2400bed95efed82eb496971b248173c5
gpablosl <69810373+gpablosl@users.noreply.github.com>
2023-02-22 19:10:46 -0700

    Add files via upload

---
+++ Practica2_ejercicio2.py
@@ -0,0 +1,21 @@
+#SUMA
+def sumar(n1, n2):
+    n3 = n1 + n2
+    print(n3)
+
+sumar(30,25)
+
+#MULTIPLICACION
+def multiplicar(n1, n2):
+    n3 = n1 * n2
+    print(n3)
+
+multiplicar(10,11)
+
+#COMPARAR LISTA
+def maximo(n1,n2,n3,n4,n5):
+    a = [n1, n2, n3, n4, n5]
+    print(max(a))
+
+(maximo(1,99,69,9,32))
+
