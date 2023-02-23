65d8e191ca3674f26776ceb7b1fd9295e1ec74c4
gpablosl <69810373+gpablosl@users.noreply.github.com>
2023-02-22 19:31:58 -0700

    Update Practica2_ejercicio2.py

--- Practica2_ejercicio2.py
+++ Practica2_ejercicio2.py
@@ -1,20 +1,23 @@
+import math
 #SUMA
-def sumar(n1, n2):
-    n3 = n1 + n2
-    print(n3)
+lista=[11,8,10,2,5,4,6]
 
-sumar(30,25)
+def sumar(lista):
+    x = sum(lista)
+    print(x)
+
+sumar(lista)
 
 #MULTIPLICACION
 def multiplicar(n1, n2):
-    n3 = n1 * n2
-    print(n3)
+    x = math.prod(lista)
+    print(x)
 
 multiplicar(10,11)
 
 #COMPARAR LISTA
-def compara_lista(n1,n2,n3,n4,n5):
-    a = [n1, n2, n3, n4, n5]
-    print(max(a))
+def comparar_lista(lista):
+    x = max(lista)
+    print(x)
 
-(compara_lista(1,99,69,9,32))
+comparar_lista(lista)
