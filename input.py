aa0807cb13d942ae9720fb3f4df76007e01fa280
gpablosl <69810373+gpablosl@users.noreply.github.com>
2023-02-26 17:08:12 -0700

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+print('Introduce la base del triangulo')
+x = float(input())
+print('Introduce la altura del triangulo') 
+y = float(input())
+
+z = (x*y)/2
+print('El area del triangulo es: ') 
+print(z)
